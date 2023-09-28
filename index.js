261644c521b124ff7e872cc8190d7b44d9247500
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-09-28 11:13:11 +0530

    swiping

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 
-var myElement = document.getElementById('mobilewrap');
-var hammertime = new Hammer(myElement);
-hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+// this function will perform actions when swiping happens
+function swipedetect(el, callback){
 
-hammertime.on('swipeleft', function(ev) {
-  keypress(37);
-});
+    var touchsurface = el,
+    swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 50, //required min distance traveled to be considered swipe
+    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 300, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+    handleswipe = callback || function(swipedir){}
 
-hammertime.on('swiperight', function(ev) {
-  keypress(39);
-});
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        e.preventDefault()
+    }
+    , false)
 
-hammertime.on('swipeup', function(ev) {
-  keypress(38);
-});
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault() // prevent scrolling when inside DIV
+    }
+    , false)
 
-hammertime.on('swipedown', function(ev) {
-  keypress(40);
-});
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if(elapsedTime <= allowedTime){ // first condition for awipe met
+            if(Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+            }
+            else if(Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        handleswipe(swipedir)
+        e.preventDefault()
+    }
+    , false)
+}
+// this function will perform actions when swiping happens
+var el = document.getElementById('ul_grid')
+swipedetect(el, function(swipedir){
+    // swipedir contains either "none", "left", "right", "top", or "down"
+    if(swipedir =='left'){
+        left()
+    }
+    else if(swipedir =='right'){
+        right()
+    }
+    else if(swipedir =='up'){
+        up()
+    }
+    else if(swipedir =='down'){
+        down()
+    }
+})
+
 
 
 
